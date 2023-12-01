6efe28023130d811576cb901801ba41537cd1dac
sunba <sunba2318@gmail.com>
2023-12-01 19:03:46 +0900

    repository 패턴 사용

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { MemberEntity } from '../libs/database/entities/member.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(MemberEntity)
+    private readonly memberRepository: Repository<MemberEntity>,
+  ) {}
   getHello(): string {
     return 'Hello World!';
   }
